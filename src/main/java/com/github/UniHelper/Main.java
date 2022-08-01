04517c7addeca12fe184a545a7356e224804c24e
Capybara666 <damian.niemczyk314@gmail.com>
2022-08-01 13:46:17 +0200

    Add project files

---
+++ src/main/java/com/github/UniHelper/Main.java
@@ -0,0 +1,7 @@
+package com.github.UniHelper;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/github/UniHelper/MainTest.java
@@ -0,0 +1,11 @@
+package com.github.UniHelper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    void mainTest() {
+        Assertions.assertEquals(1, 1);
+    }
+}
